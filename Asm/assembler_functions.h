0d35a1746d05175dcc2dadaf7381a13be0c02cab
Zinawe <121714420+KostKost777@users.noreply.github.com>
2025-10-24 20:10:49 +0300

    Add files via upload

---
+++ Asm/assembler_functions.h
@@ -0,0 +1,43 @@
+#ifndef TRANSLATOR
+#define TRANSLATOR
+
+const size_t LABELS_ARR_CAPACITY = 100;
+
+struct StructLabel
+{
+    size_t label_hash;
+    char* label_name;
+    int label_pc;
+};
+
+struct StructLabelsArr
+{
+    struct StructLabel arr[LABELS_ARR_CAPACITY];
+    size_t size;
+    int compile_stage;
+};
+
+struct CurrentStateAsm
+{
+    struct Buffer* buffer;
+    int* pc;
+    struct StructCmd cmd_struct;
+};
+
+extern const char* source_file_name;
+
+extern const char* const reg_name_arr[NUMBER_OF_REGS];
+
+int AsmPrintLogs(const char* message, size_t line);
+
+int AsmWriteCmdInFile(struct Buffer* buffer);
+
+int AsmWriteCmdInBinFile(struct Buffer* buffer);
+
+int AsmReadCmdFromFile(struct Buffer* buffer);
+
+void AsmEndProcessing(struct Buffer* buffer);
+
+int StructCmdComparatorByHash(const void* param1, const void* param2);
+
+#endif
---
+++ Asm/assembler_main.cpp
@@ -0,0 +1,54 @@
+#include<TXLib.h>
+
+#include "STACK\stack_functions.h"
+#include "COMMON\common_functions.h"
+#include "STACK\dump_functions.h"
+
+#include "MC_Onegin\read_poem_from_file_functions.h"
+#include "MC_Onegin\clean_memory_functions.h"
+
+#include "assembler_functions.h"
+
+int main(int argc, char* argv[])
+{
+    atexit(CloseLogFile);
+
+    if (argc == 1)
+        source_file_name = "../SOURCE/source.asm";
+
+    else if (argc >= 2)
+        source_file_name = argv[1];
+
+    struct Buffer buffer = {};
+
+    qsort(all_cmd, NUM_OF_CMDS, sizeof(StructCmd), StructCmdComparatorByHash);
+
+    // for (int i = 0; i < NUM_OF_CMDS; ++i) {
+    //     printf("STR: %s HASH: %u\n", all_cmd[i].name, all_cmd[i].hash);
+    // }
+
+    if (BufferCtor(&buffer)) {
+        AsmEndProcessing(&buffer);
+        return 1;
+    }
+
+    if (AsmReadCmdFromFile(&buffer)) {
+        AsmEndProcessing(&buffer);
+        return 1;
+    }
+
+    if (AsmWriteCmdInFile(&buffer)) {
+        AsmEndProcessing(&buffer);
+        return 1;
+    }
+
+    if (AsmWriteCmdInBinFile(&buffer)) {
+        AsmEndProcessing(&buffer);
+        return 1;
+    }
+
+    AsmEndProcessing(&buffer);
+
+    printf("Asm OK\n");
+
+}
